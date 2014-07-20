f78f7ae97820c3e8bdb075ab962771d7363ddfcf
David Logsdon <dlogs@live.com>
2014-07-19 23:43:19 -0700

    added test repository and various views

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var app = express();
+var repo = require('./testRepository');
 
 app.set('view engine', 'vash');
 
 
 app.get('/', function(req, res) {
-	res.render('test.vash')
+	res.render('index.vash', { title : 'Rdio Thing'});
 	res.end();
-})
+});
+
+app.get('/:page', function(req, res) {
+	//TODO: hit the db
+	res.render('party.vash', {
+		title : "Ben's Party",
+		name : "Ben's Party",
+		votedTracks : repo.getVotedTracks()
+	})
+	res.end();
+});
 
 app.use(express.static('public'));
 app.listen(3000);
