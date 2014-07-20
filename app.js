d5a5afa69949a7cbed740e4e0d2df12dc7ec2a56
Sammons <bsammnz@gmail.com>
2014-07-19 23:35:30 -0500

    added vash rendering

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = express();
 
 app.set('view engine', 'vash');
 
+
+app.get('/', function(req, res) {
+	res.render('test.vash')
+	res.end();
+})
+
 app.use(express.static('public'));
-
-
 app.listen(3000);
