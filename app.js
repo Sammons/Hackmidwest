269d4a2223f5e20745bfc4d155d3851b25d5242a
Sammons <bsammnz@gmail.com>
2014-07-20 01:59:03 -0500

    adding database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cookieparser = require('cookie-parser')
 var session = require('express-session');
 var init_rdio = require('./initialize_rdio.js');
+var database = require('./database.js')
 var app = express();
 app.use(session({secret: "sosecret", resave: true, saveUninitialized: true}))
 app.set('port', process.env.PORT || 3000)
